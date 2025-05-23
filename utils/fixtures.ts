2692c69112c2792c9719d5c1e113a38ee042d5ca
rhojane <rhojane@misfit-inc.com>
2025-05-24 02:15:04 +0800

    Updated fixtures.ts and accountactivation.spec.ts with new functionality

---
+++ utils/fixtures.ts
@@ -0,0 +1,12 @@
+import { test as base, Page } from '@playwright/test';
+
+
+export async function adminLogin(page: Page) {
+    await page.goto('https://ascendra-portal-staging.azurewebsites.net/login');
+    await page.getByRole('textbox', { name: 'Username' }).click();
+    await page.getByRole('textbox', { name: 'Username' }).fill('admin');
+    await page.getByRole('textbox', { name: 'Username' }).press('Tab');
+    await page.getByRole('textbox', { name: 'Password' }).fill('a$c3ndr@_Adm!N');
+    await page.getByRole('button', { name: 'Login' }).click();
+  }
+  
