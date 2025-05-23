a681c38fbb30336f3c8cb32c0a8ab88bca90df6d
Vegie <vegie@skunkworks.ai>
2025-05-24 02:15:04 +0800

    Updated fixtures.ts and accountactivation.spec.ts with new functionality

---
+++ utils/signup.ts
@@ -0,0 +1,36 @@
+import { Page } from '@playwright/test';
+import { faker } from '@faker-js/faker';
+
+export async function signup(page: Page) {
+  const firstName = faker.person.firstName();
+  const middleName = faker.person.middleName();
+  const lastName = faker.person.lastName();
+  const birthday = faker.date.birthdate({ min: 18, max: 50, mode: 'age' }).toISOString().split('T')[0];
+  const email = faker.person.firstName();
+  const address = faker.location.streetAddress();
+
+  await page.goto('https://ascendra-portal-staging.azurewebsites.net/');
+  await page.getByRole('link', { name: 'Signup' }).click();
+
+  await page.getByRole('textbox', { name: 'First Name *' }).fill(firstName);
+  await page.getByRole('textbox', { name: 'Middle Name' }).fill(middleName);
+  await page.getByRole('textbox', { name: 'Last Name *' }).fill(lastName);
+  await page.getByRole('textbox', { name: 'Birthday *' }).fill(birthday);
+
+  await page.getByRole('combobox').filter({ hasText: 'Gender' }).click();
+  await page.getByRole('option', { name: 'Male', exact: true }).click();
+
+  await page.getByRole('combobox').filter({ hasText: 'Status' }).click();
+  await page.getByRole('option', { name: 'Single' }).click();
+
+  await page.getByRole('textbox', { name: 'Email Address *' }).fill('vegie+' + email + '@skunkworks.ai');
+  await page.getByRole('textbox', { name: 'Address *', exact: true }).fill(address);
+  await page.getByRole('textbox', { name: 'Sponsor ID *' }).fill('ASC-8503045209');
+
+  await page.getByRole('button', { name: 'Next' }).click();
+  await page.getByRole('button', { name: 'Choose Package' }).first().click();
+  await page.getByRole('button', { name: 'Next' }).click();
+  await page.getByText('Terms and Condition').click();
+  await page.getByRole('button', { name: 'Pay Now' }).click();
+  await page.getByRole('button', { name: 'Understood' }).click();
+}
